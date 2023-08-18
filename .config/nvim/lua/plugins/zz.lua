e9b4deea4ca84e3424ed5be0d757941ca2e4d354
Michael Gazzola <michaelagazzola@gmail.com>
2023-08-17 21:51:49 -0400

    [add] nvim: ctrl-j/k scrolling

--- .config/nvim/lua/plugins/zz.lua
+++ .config/nvim/lua/plugins/zz.lua
@@ -1,4 +1,32 @@
 return {
+  {
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      })
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-j>"] = function(...)
+              return require("telescope.actions").move_selection_next(...)
+            end,
+            ["<C-k>"] = function(...)
+              return require("telescope.actions").move_selection_previous(...)
+            end,
+          },
+        },
+      },
+    },
+  },
   -- Copilot
   {
     "zbirenbaum/copilot.lua",
