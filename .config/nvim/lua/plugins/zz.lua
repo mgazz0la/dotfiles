d3a0d613b19855a6077b0dec5e7ce00ba1dce8b0
Michael Gazzola <michaelagazzola@gmail.com>
2023-08-19 13:50:13 -0400

    [fix] nvim: don't overwrite existing nvim-cmp keybinds

--- .config/nvim/lua/plugins/zz.lua
+++ .config/nvim/lua/plugins/zz.lua
@@ -4,9 +4,9 @@ return {
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require("cmp")
-      opts.mapping = cmp.mapping.preset.insert({
-        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
       })
     end,
   },
